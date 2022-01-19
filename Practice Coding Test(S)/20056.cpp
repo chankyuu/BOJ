d886fa06dfd1c73c19ef9f42cea37f7ee6115597
chankyuu <60462925+chankyuu@users.noreply.github.com>
2022-01-19 13:53:56 +0900

    마법사 상어와 파이어볼 [20056]

--- Practice Coding Test(S)/20056.cpp
+++ Practice Coding Test(S)/20056.cpp
@@ -11,56 +11,53 @@ int dx[8] = { -1, -1, 0, 1, 1, 1, 0, -1 };
 int dy[8] = { 0, 1, 1, 1, 0, -1, -1, -1 };
 
 int N, M, K;
-int board[52][52];
 
 struct fireBall {
-	int r = 0;					// ÆÄÀÌ¾îº¼ À§Ä¡(Çà)
-	int c = 0;					// ÆÄÀÌ¾îº¼ À§Ä¡(¿­)
-	int m = 0;					// Áú·®
-	int s = 0;					// ¼Ó·Â
-	int d = 0;					// ¹æÇâ
+	int r = 0;					// íŒŒì´ì–´ë³¼ ìœ„ì¹˜(í–‰)
+	int c = 0;					// íŒŒì´ì–´ë³¼ ìœ„ì¹˜(ì—´)
+	int m = 0;					// ì§ˆëŸ‰
+	int s = 0;					// ì†ë ¥
+	int d = 0;					// ë°©í–¥
 };
 
+vector<fireBall> board[52][52];
 vector<fireBall> fireBalls;
 
-// ÆÄÀÌ¾îº¼ ÀÌµ¿
+// íŒŒì´ì–´ë³¼ ì´ë™
 void moveFire() {
+	for (int i = 0; i < N; i++) for (int j = 0; j < N; j++) board[i][j].clear();
+
 	for (int i = 0; i < fireBalls.size(); i++) {
 		fireBall fb = fireBalls[i];
-		for (int j = 1; j <= fb.s; j++) {
-			fb.r += dx[fb.d];
-			fb.c += dy[fb.d];
+		fb.r += dx[fb.d] * fb.s;
+		fb.c += dy[fb.d] * fb.s;
 
-			if (fb.r < 0)  fb.r = N - 1;
-			if (fb.r >= N) fb.r = 0;
-			if (fb.c < 0) fb.c = N - 1;
-			if (fb.c >= N) fb.c = 0;
-		}
+		while (fb.r < 0)  fb.r += N;
+		while (fb.r >= N) fb.r -= N;
+		while (fb.c < 0) fb.c += N;
+		while (fb.c >= N) fb.c -= N;
+		
 		fireBalls[i].r = fb.r;
 		fireBalls[i].c = fb.c;
-		board[fb.r][fb.c]++;
+		board[fb.r][fb.c].push_back(fb);
 	}
-	// ÆÄÀÌ¾îº¼ ÇÕÄ¡°í »èÁ¦
+	// íŒŒì´ì–´ë³¼ í•©ì¹˜ê³  ì‚­ì œ
+	vector<fireBall> tmp;
+
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
-			if (board[i][j] >= 2) {
+			if (board[i][j].size() == 1) tmp.push_back(board[i][j][0]);
+			if (board[i][j].size() >= 2) {
 				vector<bool> t;
-				fireBall tmp;
+				int mass = 0;
+				int speed = 0;
 
-				tmp.r = i;
-				tmp.c = j;
+				for (int k = 0; k < board[i][j].size(); k++) {
+					mass += board[i][j][k].m;
+					speed += board[i][j][k].s;
 
-				for (int k = 0; k < fireBalls.size(); k++) {
-					if (i == fireBalls[k].r && j == fireBalls[k].c) {
-						tmp.m += fireBalls[k].m;
-						tmp.s += fireBalls[k].s;
-
-						if(fireBalls[k].d % 2 == 0) t.push_back(true);
-						else t.push_back(false);
-
-						fireBalls.erase(fireBalls.begin() + k);
-						k--;
-					}
+					if(board[i][j][k].d % 2 == 0) t.push_back(true);
+					else t.push_back(false);
 				}
 				bool flag = true;
 				for (int k = 1; k < t.size(); k++) {
@@ -70,53 +67,26 @@ void moveFire() {
 						break;
 					}
 				}
-				tmp.m /= 5;
-				tmp.s /= board[i][j];
+				mass /= 5;
+				speed /= board[i][j].size();
 
-				if (tmp.m == 0) {
-					board[i][j] = 0; 
-					continue;
-				}
+				if (mass == 0) 	continue;
+				
 				if (flag) {
 					for (int dir = 0; dir < 8; dir += 2) {
-						pair<int, int> cur = { i, j };
-						tmp.d = dir;
-						cur.X += dx[tmp.d];
-						cur.Y += dy[tmp.d];
-
-						if (cur.X < 0)  cur.X = N - 1;
-						if (cur.X >= N) cur.X = 0;
-						if (cur.Y < 0) cur.Y = N - 1;
-						if (cur.Y >= N) cur.Y = 0;
-
-						tmp.r = cur.X;
-						tmp.c = cur.Y;
-						fireBalls.push_back(tmp);
+						tmp.push_back({ i, j, mass, speed, dir });
 					}
 				}
 				else {
 					for (int dir = 1; dir < 8; dir += 2) {
-						pair<int, int> cur = { i,j };
-						tmp.d = dir;
-						cur.X += dx[tmp.d];
-						cur.Y += dy[tmp.d];
-
-						if (cur.X < 0)  cur.X = N - 1;
-						if (cur.X >= N) cur.X = 0;
-						if (cur.Y < 0) cur.Y = N - 1;
-						if (cur.Y >= N) cur.Y = 0;
-
-						tmp.r = cur.X;
-						tmp.c = cur.Y;
-
-						fireBalls.push_back(tmp);
+						tmp.push_back({ i, j, mass, speed, dir });
 					}
 				}
-				board[i][j] = 0;
 			}
 		}
 	}
-	cout << fireBalls.size();
+	fireBalls = tmp;
+	//cout << fireBalls.size();
 }
 int main() {
 	ios::sync_with_stdio(0);
@@ -133,8 +103,6 @@ int main() {
 		fb.c -= 1;
 		fireBalls.push_back(fb);
 	}
-
-	for (int i = 0; i < N; i++) fill(board[i], board[i] + N, 0);
 
 	while (K--) {
 		moveFire();
