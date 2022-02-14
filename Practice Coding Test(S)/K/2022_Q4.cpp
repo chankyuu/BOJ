ec4baf2222b6edc1c305ec64bc8c41181cfb1cb0
chankyuu <60462925+chankyuu@users.noreply.github.com>
2022-02-14 19:12:04 +0900

    2022_Q4 *
    백트래킹
    next_permutation

--- Practice Coding Test(S)/K/2022_Q4.cpp
+++ Practice Coding Test(S)/K/2022_Q4.cpp
@@ -1,59 +1,98 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-vector<int> lion(11);
-vector<int> appeach;
-vector<int> comp;
-int pre = 0;
-// È­»ì ½î±â
-void count(int n, int cnt) {
-	if (n == 10) {
-		lion[n] = cnt;
-		// Á¡¼ö°è»ê
-		int result = 0;
-		for (int i = 0; i < 11; i++) {
-			if (lion[i] > appeach[i]) result += (10 - i);
-			else if (appeach[i] != 0) result -= (10 - i);
-		}
-		if (result <= 0) return;
-
-		if (comp.size() == 0) {
-			comp = lion;
-			pre = result;
-			return;
-		}
-		if (result > pre) {
-			pre = result;
-			comp = lion;
-			return;
-		}
-		else if (result == pre) {
-			for (int i = 10; i >= 0; i--) {
-				if (comp[i] < lion[i]) {
-					comp = lion;
-					return;
-				}
-				else if (comp[i] > lion[i]) return;
-			}
-		}
-		return;
-	}
-
-	for (int i = 0; i <= cnt; i++) {
-		lion[n] = i;
-		count(n + 1, cnt - i);
-	}
-}
+//vector<int> lion(11);
+//vector<int> appeach;
+//vector<int> comp;
+int result = 0;
+// í™”ì‚´ ì˜ê¸°(ë°±íŠ¸ë˜í‚¹)
+//void count(int n, int cnt) {
+//	if (n == 10) {
+//		lion[n] = cnt;
+//		// ì ìˆ˜ê³„ì‚°
+//		int result = 0;
+//		for (int i = 0; i < 11; i++) {
+//			if (lion[i] > appeach[i]) result += (10 - i);
+//			else if (appeach[i] != 0) result -= (10 - i);
+//		}
+//		if (result <= 0) return;
+//
+//		if (comp.size() == 0) {
+//			comp = lion;
+//			pre = result;
+//			return;
+//		}
+//		if (result > pre) {
+//			pre = result;
+//			comp = lion;
+//			return;
+//		}
+//		else if (result == pre) {
+//			for (int i = 10; i >= 0; i--) {
+//				if (comp[i] < lion[i]) {
+//					comp = lion;
+//					return;
+//				}
+//				else if (comp[i] > lion[i]) return;
+//			}
+//		}
+//		return;
+//	}
+//
+//	for (int i = 0; i <= cnt; i++) {
+//		lion[n] = i;
+//		count(n + 1, cnt - i);
+//	}
+//}
 vector<int> solution(int n, vector<int> info) {
 	vector<int> answer;
 
-	appeach = info;
-	count(0, n);
+	// ë°±íŠ¸ë˜í‚¹(count í•¨ìˆ˜ ì´ìš©)
+	//count(0, n); 
 
-	if (comp.size() == 0) answer.push_back(-1);
-	else answer = comp;
+	// next_permutation ì´ìš©
+	vector<int> lion(11, -1);
+	vector<int> brute(n + 10);
+
+	fill(brute.begin() + n, brute.end(), 1);
+
+	do {
+		vector<int> arrow;
+		int pre = -1;
+		for (int i = 0; i < n + 10; i++) {
+			if (brute[i] == 1) {
+				arrow.push_back(i - pre - 1);
+				pre = i;
+			}
+		}
+		arrow.push_back(n + 10 - pre - 1);
+		int score = 0;
+		for (int i = 0; i < 11; i++) {
+			if (arrow[i] > info[i]) score += (10 - i);
+			else if (info[i] != 0) score -= (10 - i);
+		}
+		if (score <= 0) continue;
+
+		if (score > result) {
+			lion = arrow;
+			result = score;
+		}
+		else if (score == result) {
+			for (int i = 10; i >= 0; i--) {
+				if (lion[i] < arrow[i]) {
+					lion = arrow;
+					break;
+				}
+				else if (lion[i] > arrow[i]) break;
+			}
+		}
+	} while (next_permutation(brute.begin(), brute.end()));
+
+	if (result == 0) answer.push_back(-1);
+	else answer = lion;
 	return answer;
 }
